0e447916b7ced8d2da1f324822a7255dfd2ec8a1
yihong0618 <zouzou0208@gmail.com>
2022-08-25 13:35:52 +0800

    fix: api broken

--- duolingo.py
+++ duolingo.py
@@ -1,14 +1,26 @@
 import argparse
 import os
-from getpass import getpass
-from random import shuffle
+import json
 
 import requests
 
+DUOLINGO_SETTING_URL = "https://www.duolingo.com/api/1/version_info"
+
+
+def get_duolingo_setting():
+    try:
+        r = requests.get(DUOLINGO_SETTING_URL)
+    except Exception as e:
+        print(f"Something is wrong to get the setting error: {str(e)}")
+        exit(1)
+    setting_data = r.json()
+    tts_base_url = setting_data.get("tts_base_url")
+    if not tts_base_url:
+        raise Exception("Something wrong get the tts url")
+    lauguage_tts_dict = setting_data.get("tts_voice_configuration", {}).get("voices")
+    return tts_base_url, json.loads(lauguage_tts_dict)
 
 def get_duolingo_session_and_name(user_name, password):
-    if password is None:
-        password = getpass()
     s = requests.Session()
     r = s.post(
         "https://www.duolingo.com/login",
@@ -25,32 +37,30 @@ def get_duolingo_daily(session, name):
     if r.status_code != 200:
         raise Exception("Get profile failed")
     data = r.json()
-
     is_today_check = data["streak_extended_today"]
     streak = data["site_streak"]
     lauguage = data["learning_language"]
     level_progress = data["language_data"].get(lauguage, {}).get("level_progress", 0)
-    return level_progress, streak, is_today_check
+    return level_progress, lauguage, streak, is_today_check
 
 
-def get_duolingo_words_and_save_mp3(session, latest_num=100):
+def get_duolingo_words_and_save_mp3(session, tts_url, latest_num=100):
     r = session.get("https://www.duolingo.com/vocabulary/overview")
     if not r.ok:
         raise Exception("get duolingo words failed")
+    
     words = r.json()["vocab_overview"]
+    words.sort(key=lambda v: v.get("last_practiced_ms", 0), reverse=True)
     words_list = []
     i = 1
     my_new_words = words[:latest_num]
-    shuffle(my_new_words)
     for w in my_new_words:
         if w["normalized_string"] == "<*sf>":
             continue
-        words_list.append(w["word_string"])
+        word_string = w["word_string"] 
+        words_list.append(word_string)
         try:
-            word_info = session.get(
-                f"https://www.duolingo.com/api/1/dictionary_page?lexeme_id={w['lexeme_id']}"
-            ).json()
-            mp3_content = requests.get(word_info["tts"])
+            mp3_content = requests.get(f"{tts_url}{word_string}")
             with open(os.path.join("MP3_NEW", str(i) + ".mp3"), "wb") as f:
                 f.write(mp3_content.content)
             i += 1
@@ -64,14 +74,17 @@ def main(duolingo_user_name, duolingo_password, tele_token, tele_chat_id, latest
     s, duolingo_name = get_duolingo_session_and_name(
         duolingo_user_name, duolingo_password
     )
-    _, duolingo_streak, duolingo_today_check = get_duolingo_daily(s, duolingo_name)
+    _, lauguage, duolingo_streak, duolingo_today_check = get_duolingo_daily(s, duolingo_name)
+    tts_url_base, lauguage_setting_dict = get_duolingo_setting()
+    lauguage_path = lauguage_setting_dict.get(lauguage) 
+    tts_url = f"{tts_url_base}tts/{lauguage_path}/token/"
     try:
         latest_num = int(latest_num)
     except Exception as e:
         print(str(e))
         # default
         latest_num = 50
-    duolingo_words = get_duolingo_words_and_save_mp3(s, latest_num=latest_num)
+    duolingo_words = get_duolingo_words_and_save_mp3(s, tts_url, latest_num=latest_num)
     if duolingo_words:
         duolingo_words = (
             f"Your streak: {duolingo_streak}\n" "New words\n" + duolingo_words
